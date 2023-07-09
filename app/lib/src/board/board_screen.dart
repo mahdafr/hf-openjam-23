9f3b47957bb9f372ec6101b3e9bde8ba4f21eec7
gcervantes8 <gercervantes7@gmail.com>
2023-07-09 07:25:14 -0400

    Added player movement!!

--- app/lib/src/board/board_screen.dart
+++ app/lib/src/board/board_screen.dart
@@ -94,7 +94,7 @@ class _MinesweeperScreen extends State<MinesweeperScreen>
     final offset =
         details.globalPosition - renderBox.localToGlobal(Offset.zero);
     final scenePoint = _transformationController.toScene(offset);
-    // final boardPoint = _board.pointToBoardPoint(scenePoint);
+    _board.pointToDirection(scenePoint);
     setState(() {
       // _board = _board.copyWithSelected(boardPoint);
     });
@@ -228,7 +228,12 @@ class _BoardPainter extends CustomPainter {
     drawPlayers(board.dogPlayers);
     int playerId = 0;
     for (Doggo player in board.dogPlayers) {
-      Vector2 vectorMovement = calculateMove(playerId, players, Strategy.smart);
+      Vector2 vectorMovement = Vector2(0, 0);
+      if (player.strategy == Strategy.none) {
+        vectorMovement = board.playerDirection;
+      } else {
+        vectorMovement = calculateMove(playerId, players, Strategy.smart);
+      }
       // List<double> newCoords = moveRandomly(player, board.dogPlayers);
 
       //TODO from unit vector, use velocity to find new position
@@ -241,6 +246,9 @@ class _BoardPainter extends CustomPainter {
     // Collision checking
     for (Doggo player1 in board.dogPlayers) {
       for (Doggo player2 in board.dogPlayers) {
+        if (player1 == player2) {
+          continue;
+        }
         // Finding collision between 2 players can be done by finding the
         // smaller player, getting the unit vector, multiplying it by it's size,
         // and then checking if that position is inside the larger player
@@ -252,32 +260,63 @@ class _BoardPainter extends CustomPainter {
         }
         Vector2 unitVec =
             getUnitVector(smallerPlayer.position, largerPlayer.position);
-        Vector2 smallerPlayerEdge = unitVec * smallerPlayer.size;
+        Vector2 smallerPlayerMagnitude = unitVec * smallerPlayer.size;
+        Vector2 smallerPlayerEdge =
+            smallerPlayer.position + smallerPlayerMagnitude;
         num circleEquation = pow(smallerPlayerEdge.x - largerPlayer.x, 2) +
-            pow(smallerPlayerEdge.x - largerPlayer.x, 2);
+            pow(smallerPlayerEdge.y - largerPlayer.y, 2);
 
         if (circleEquation < largerPlayer.size) {
           collision(smallerPlayer, largerPlayer);
         }
       }
     }
+    addPoints(players, 0.0002);
+    if (isWinner(board.dogPlayers, 500)) {}
   }
 
-  void collision(Doggo player1, player2) {}
+  bool isWinner(List<Doggo> dogPlayers, double winningScore) {
+    for (Doggo player in dogPlayers) {
+      if (player.size > winningScore) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void collision(Doggo player1, Doggo player2) {
+    void playerEat(Doggo playerWhoAte, Doggo playerEaten) {
+      killPlayer(playerEaten);
+      double playerArea = pi * pow(playerWhoAte.size, 2);
+      double areaWon = pi * pow(playerEaten.size, 2);
+      double newRadius = sqrt((playerArea + areaWon) / pi);
+      playerWhoAte.size = newRadius;
+    }
+
+    if (player1.size < player2.size) {
+      playerEat(player2, player1);
+    } else {
+      playerEat(player1, player2);
+    }
+  }
+
+  void killPlayer(Doggo player) {
+    var rng = Random();
+    player.position.x = rng.nextInt(1000).toDouble();
+    player.position.y = rng.nextInt(1000).toDouble();
+  }
+
+  void addPoints(List<Doggo> dogPlayers, double points) {
+    for (Doggo player in dogPlayers) {
+      player.size += points;
+    }
+  }
 
   // We should repaint whenever the board changes, such as board.selected.
   @override
   bool shouldRepaint(_BoardPainter oldDelegate) {
     return true;
   }
-}
-
-List<double> moveRandomly(Doggo player, List<Doggo> players) {
-  double xMovement = Random().nextDouble() - 0.5;
-  double yMovement = Random().nextDouble() - 0.5;
-  double newX = player.x + xMovement;
-  double newY = player.y + yMovement;
-  return [newX, newY];
 }
 
 // The entire state of the hex board and abstraction to get information about
@@ -301,13 +340,26 @@ class Board extends Object {
   // final double rectRadius; // Pixel radius of a rectangle (center to vertex).
   // final double rectMargin; // Margin between cells.
   final List<Doggo> dogPlayers = players;
+  Vector2 playerDirection = Vector2(0, 0);
 
   // Get the size in pixels of the entire board.
   Size get size {
     return Size(boardWidth + 30, boardHeight + 30);
   }
 
-  void forcePointOnBoard(int x, int y) {}
+  Doggo findPlayer() {
+    for (Doggo player in dogPlayers) {
+      if (player.strategy == Strategy.none) {
+        return player;
+      }
+    }
+    return dogPlayers[0];
+  }
+
+  void pointToDirection(Offset point) {
+    Vector2 targetDirection = Vector2(point.dx, point.dy);
+    playerDirection = getUnitVector(findPlayer().position, targetDirection);
+  }
 
   bool isValidPoint(int x, int y) {
     return !(x < 0 || y < 0 || x >= boardWidth || y >= boardHeight);
