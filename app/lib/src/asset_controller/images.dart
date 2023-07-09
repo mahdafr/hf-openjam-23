dc99e59f0746be831c217f2280d1cc9080a92e1b
mahdafr <mmafravi@gmail.com>
2023-07-09 07:39:03 -0400

    refactored image assets to be ready for game mechanics

--- app/lib/src/asset_controller/images.dart
+++ app/lib/src/asset_controller/images.dart
@@ -1,9 +1,10 @@
-final List<String> imgList = [
-  'assets/images/dogs/husk0.png',
-  'assets/images/dogs/chih0.png',
-  'assets/images/dogs/pug0.png',
-  // 'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-  // 'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-  // 'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+const String pref = 'assets/images/dogs/';
+const String suf = '0.png';
+
+final List<String> rtImgList = [
+  'husk',
+  'chih',
+  'pug',
 ];
 
+var imgList = rtImgList.map((val) => '$pref$val$suf').toList();
