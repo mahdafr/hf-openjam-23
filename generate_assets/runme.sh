2af2596f605ba7edcba425b5009322351052c697
mahdafr <mmafravi@gmail.com>
2023-07-09 04:16:04 -0400

    WSL and docker pls

--- generate_assets/runme.sh
+++ generate_assets/runme.sh
@@ -1,10 +1,14 @@
 echo "BUILDING DOCKER IMAGE"
-sudo sh dockerized_stablediff/build.sh pull;
+cp token.txt dockerized_stablediff/;
+cd dockerized_stablediff/;
+sudo sh build.sh pull;
+sudo sh build.sh build;
+cd ..;
 echo "======================================="
 
 echo "GENERATING OPTIONS FOR IMAGES"
-STR=$'siberian husky\ngerman shepherd\nchihuahua\nlabrador retriever\npug\npoodle';
-echo "$STR" >> doglist.txt;
+STR='siberian husky\ngerman shepherd\nchihuahua\nlabrador retriever\npug\npoodle';
+echo "$STR" > doglist.txt;
 echo "======================================="
 
 echo "GENERATING SINGLE SOURCE IMAGES"
@@ -14,7 +18,7 @@ done <doglist.txt
 echo "======================================="
 
 echo "GENERATING PERMUTATION SOURCE IMAGES"
-parallel 'bash -c "[ {1} != {2} ] && echo {1} mixed with {2} dog"' :::: doglist.txt doglist.txt >> dogperms.txt;
+parallel 'bash -c "[ {1} != {2} ] && echo {1} dog mixed with {2} dog"' :::: doglist.txt doglist.txt > dogperms.txt;
 while read p; do
     sudo sh dockerized_stablediff/build.sh run --model 'stabilityai/stable-diffusion-2-1' "$p";
 done <dogperms.txt
